a6f7bae92a96189d0890f350bd588fabcc3ab15d
B1twis3 <b1twise@protonmail.com>
2017-08-20 00:19:51 +0200

    Added the os module to fetch the username

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 console.log('starting app');
 
+// using require to add modules
 const fs = require('fs');
+const os = require('os');
 
-fs.appendFile('greetings.txt', 'Hello world!', function (err) {
+// add user variable to store user information
+var user = os.userInfo();
+
+// display information stored
+console.log(user);
+
+fs.appendFile('greetings.txt', `Hello {user.username}!` ,function (err) {
   if (err) {
     console.log('Unable to write to file');
   }
