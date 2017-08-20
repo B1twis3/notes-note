c0c53744174f40484d2e36a613e96993f1aa1306
B1twis3 <b1twise@protonmail.com>
2017-08-20 19:55:36 +0200

    Added dummy addNote function

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
 
+result = notes.addNote();
+console.log(result);
+
 // add user variable to store user information;
-var user = os.userInfo();
+// var user = os.userInfo();
 
 // // display information stored;
 // console.log(user);
 
 // use filesystem module with error handling callback;
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} years old.` ,function (err) {
-  if (err) {
-    console.log('Unable to write to file');
-  }
-  console.log('The data was append to file');
-});
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} years old.` ,function (err) {
+//   if (err) {
+//     console.log('Unable to write to file');
+//   }
+//   console.log('The data was append to file');
+// });
 
 /*
 // same way of using a callback for error handling done differently
--- notes.js
+++ notes.js
@@ -3,5 +3,10 @@ console.log('Starting notes.js');
 // Show all atributes of notes.js
 // console.log(module);
 
-// Add atribute age as export
-module.exports.age = 24;
+// // Add atribute age as export
+// module.exports.age = 24;
+
+module.exports.addNote = () => {
+    console.log('Add note');
+    return 'New note';
+};
