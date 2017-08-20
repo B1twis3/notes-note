9e02f58cf7fab95513353b9849908a1b1e1eb943
B1twis3 <b1twise@protonmail.com>
2017-08-20 18:24:33 +0200

    Returned to original error handlling

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ var user = os.userInfo();
 // display information stored;
 console.log(user);
 
-/*
 // use filesystem module with error handling callback;
 fs.appendFile('greetings.txt', `Hello ${user.username}!` ,function (err) {
   if (err) {
     console.log('Unable to write to file');
   }
 });
-*/
 
+/*
 // same way of using a callback for error handling done differntly
 fs.appendFile('greetings.txt', `Hello ${user.username}!`, (err) => {
   if (err) throw  err;
     console.log('The data was append to file');
 });
+*/
