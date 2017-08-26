72859d90b83e2bd6f5d08e0d0ce082bb93f27b96
B1twis3 <b1twise@protonmail.com>
2017-08-26 11:44:19 +0200

    Added external utility lodash from npm

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.log('starting app.js');
 // using require to add modules;
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
 result = notes.addNote();
