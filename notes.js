26d8b6f086af88e61f768fde3ff06f3897d73c42
B1twis3 <b1twise@protonmail.com>
2017-09-17 21:09:54 +0200

    Add reading and writing notes to file and filtering duplicates

--- notes.js
+++ notes.js
@@ -1,7 +1,31 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-  console.log('Adding note ', title, body);
+  var notes = [];
+  var note = {
+    title: title,
+    body: body
+  }
+
+try {
+  var notesString = fs.readFileSync('notes-data.json');
+  notes = JSON.parse(notesString);
+} catch (e) {
+
+}
+
+// // duplicateNotes in ES6 style
+// var duplicateNotes = notes.filter((note) => notes.title === title);
+var duplicateNotes = notes.filter((note) => {
+  return note.title === title;
+});
+
+if (duplicateNotes == 0) {
+  notes.push(note);
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+}
 };
 
 var getAll = () => {
